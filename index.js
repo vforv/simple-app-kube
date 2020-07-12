07b6d65778e768936e2f58dcd7e829cbc339d9c2
Vladimir <elitelobby.com@gmail.com>
2020-07-13 01:20:37 +0200

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
     // Send the response body as "Hello World"
-    response.end('Everything again\n');
+    response.end('Cicd working?\n');
  }).listen(3000);
  
  // Console will print the message
