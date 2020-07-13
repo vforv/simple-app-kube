357aa3287ab263ce4738b64a2db3d943b26037a6
Vladimir <elitelobby.com@gmail.com>
2020-07-13 13:22:39 +0200

    back master

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
     // Send the response body as "Hello World"
-    response.end('We have \n');
+    response.end('We have something new\n');
  }).listen(3000);
  
  // Console will print the message
